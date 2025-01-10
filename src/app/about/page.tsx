686317ff103cdae566b1b155287ab0790b6d9166
mahnoorkhalid8 <mahnoorkhalid814@gmail.com>
2025-01-10 23:52:17 +0500

    update code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,9 +6,9 @@ const About = () => {
       <div className='bg-[rgb(200,150,80)] p-8 text-left rounded-2xl w-[700px]'>
       <h1 className='text-4xl font-bold mb-4' data-aos="fade-up" >About Us</h1>
           <p>Step into Confidence with Our Exclusive
-            Collection of Men’s and Women’s Footwear. From Elegant Heels to 
+            Collection of Men&apos;s and Women&apos;s Footwear. From Elegant Heels to 
             Durable Boots, Experience Comfort and Style in Every Step! Whether
-            you're heading to a formal event, a casual outing, oran adventurous
+            you&apos;re heading to a formal event, a casual outing, oran adventurous
             journey, our footwear is designed to meet every occasion. Explore a 
             range of trendy designs, premium materials, and unmatched durability
             crafted for your perfect fit. Walk with style, stride with confidence!</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HomeComponent from "@/components/HomeComponent";
 
 export default function Home() {
